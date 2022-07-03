ee3b1594a0c21c142abf53d1142884daa70ca3d0
Hwanseung Lee <hwanseung@chromium.org>
2022-07-03 12:39:51 +0900

    add dl

--- app/src/main/java/compatibility/backward/aidl/InfoService.java
+++ app/src/main/java/compatibility/backward/aidl/InfoService.java
@@ -5,6 +5,12 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.util.Log;
+
+import com.yausername.youtubedl_android.YoutubeDL;
+import com.yausername.youtubedl_android.YoutubeDLException;
+import com.yausername.youtubedl_android.YoutubeDLRequest;
+import com.yausername.youtubedl_android.mapper.VideoInfo;
 
 public class InfoService extends Service {
     private Binder binder = new IInfoService.Stub() {
@@ -17,7 +23,7 @@ public class InfoService extends Service {
         public void requestInfo(int clientVersion, String text, IInfoServiceCallback callback)
                 throws RemoteException {
             Info info = new Info();
-            info.title = text;
+            info.title = getInfo(text);
             SubInfo subInfo = new SubInfo();
             subInfo.title = text + "_sub";
             info.infos.add(subInfo);
@@ -31,5 +37,29 @@ public class InfoService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return binder;
+    }
+
+    private String getInfo(String url) {
+        try {
+            YoutubeDLRequest request = new YoutubeDLRequest(url);
+            request.addOption("-f", "best");
+            VideoInfo streamInfo = YoutubeDL.getInstance().getInfo(request);
+            Log.i("youtube_dl", "url : " + streamInfo.getFormats().get(0).getUrl());
+            Log.i("youtube_dl", "url : " + streamInfo.getUrl());
+            Log.i("youtube_dl", "format size : " + streamInfo.getFormats().size());
+            return streamInfo.getUrl();
+        } catch (YoutubeDLException | InterruptedException e) {
+            Log.i("Test", e.toString());
+        }
+        return "";
+    }
+
+    @Override
+    public void onCreate() {
+        try {
+            YoutubeDL.getInstance().init(getApplication());
+        } catch (YoutubeDLException e) {
+            Log.e("Test", "failed to initialize youtubedl-android", e);
+        }
     }
 }
